5b9d1f08d1e5a3c9a03912f8f164d131b7c66863
Rahul Saha <rahulsaha87928@gmail.com>
2023-06-08 12:58:21 +0600

    make admin to student

--- src/components/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/components/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -30,25 +30,25 @@ const ManageUsers = () => {
         })
     }
 
-    // const handelMakeAdmin= (user) => {
-    //     fetch(`http://localhost:5000/users/admin/${user._id}`, {
-    //         method: 'PATCH'
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         if(data.modifiedCount){
-    //             refetch();
-    //             Swal.fire({
-    //                 position: 'top-end',
-    //                 icon: 'success',
-    //                 title: `${user.name} is an Admin Now!`,
-    //                 showConfirmButton: false,
-    //                 timer: 1500
-    //               })
-    //         }
-    //     })
-    // }
+    const handelMakeAdmin= (user) => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
 
     return (
         <div>
