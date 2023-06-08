07adc170d221ce739b4d76f576275ae0aa070683
Rahul Saha <rahulsaha87928@gmail.com>
2023-06-08 11:52:40 +0600

    create table for the users

--- src/components/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/components/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const ManageUsers = () => {
 
@@ -10,7 +11,36 @@ const ManageUsers = () => {
 
     return (
         <div>
-            <h2>Hello from manage users: {users.length}</h2>
+            <h2 className='text-3xl font-semibold'>Total users: {users.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th className='text-center'>Serial</th>
+                            <th className='text-center'>Name</th>
+                            <th className=''>Email</th>
+                            <th className='text-center'>Role</th>
+                            <th className='text-center'>Action 1</th>
+                            <th className='text-center'>Action 2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) =>
+                                <tr key={user._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td><button className='btn btn-primary'>{user.role}</button></td>
+                                    <td><button className="btn btn-outline btn-primary">Made Instructor</button></td>
+                                    <td><button className="btn btn-outline btn-primary">Made Admin</button></td>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
